d1503e91c8b286fbfcc05bb55e87a09f6d8ffaf8
Michael <michael.loukeris@icloud.com>
2016-12-15 12:41:42 +0200

    Adding string represantation of models

--- gigfinder/gigs/models.py
+++ gigfinder/gigs/models.py
@@ -6,8 +6,14 @@ class Venue(models.Model):
     name = models.CharField(max_length=200)
     location = models.PointField()
 
+    def __str__(self):
+        return self.name
+
 
 class Event(models.Model):
     name = models.CharField(max_length=200)
     datetime = models.DateTimeField()
     venue = models.ForeignKey(Venue)
+
+    def __str__(self):
+        return "%s - %s" % (self.name, self.venue.name)
