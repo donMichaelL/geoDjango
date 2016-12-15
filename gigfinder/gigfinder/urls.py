3fa367bd12190321ead5c62d8e2b85ab7a3ef60c
Michael <michael.loukeris@icloud.com>
2016-12-15 13:17:38 +0200

    Adding Form view and html file

--- gigfinder/gigfinder/urls.py
+++ gigfinder/gigfinder/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from gigs.views import LookupView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'', LookupView.as_view(), name='lookup'),
 ]
--- gigfinder/gigs/views.py
+++ gigfinder/gigs/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic.base import View
+from django.shortcuts import render_to_response
 
-# Create your views here.
+
+class LookupView(View):
+    def get(self, request):
+        return render_to_response('gigs/lookup.html')
